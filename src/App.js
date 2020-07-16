f4a3d3d563764725790b6c397695ae079648b188
Солодов Евгений Александрович <evgenii.kgg05@gmail.com>
2020-07-16 17:18:43 +0700

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import fs from "fs";
+
+import Makeid from "./Generate";
 
 function App() {
+  const fs = require("fs");
+
+  let lyrics =
+    "But still I'm having memories of high speeds when the cops crashed\n" +
+    "As I laugh, pushin the gas while my Glocks blast\n" +
+    "We was young and we was dumb but we had heart";
+
+  // write to a new file named 2pac.txt
+  fs.writeFile("2pac.txt", lyrics, (err) => {
+    // throws an error, you could also catch it here
+    if (err) throw err;
+
+    // success case, the file was saved
+    console.log("Lyric saved!");
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Генератор пароля</p>
+      <div>
+      <Makeid />
+      </div>
     </div>
   );
 }
---
+++ src/Generate.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function Makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
+ 
+ console.log(Makeid(5));
+
+ export default Makeid;
