8d93081f4007916e3ba9833260f01c7138b8ccae
Солодов Евгений Александрович <evgenii.kgg05@gmail.com>
2020-07-20 13:02:39 +0700

    length

--- src/Generate.js
+++ src/Generate.js
@@ -1,17 +1,25 @@
+const countPassword = 200;
+const lengthPassword = 4;
 
 function Makeid(length) {
-    let result           = '';
-    let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    let charactersLength = characters.length;
-    const positionChar = (length) => Math.floor(Math.random() * length)
-    for ( let i = 0; i < length; i++ ) {  
-        if (i === (length - 1)) {
-            const dobbleChar =  result.charAt(positionChar(i-1))
-            const resultSplit = result.split('')
-            resultSplit.splice(positionChar(i), 0 , dobbleChar)
-            result = resultSplit.join('')
-            
-        } else result += characters.charAt(positionChar(charactersLength));
-    }
-    return result;
- }
+  let result = "";
+  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let charactersLength = characters.length;
+  const positionChar = (length) => Math.floor(Math.random() * length);
+  for (let i = 0; i < length; i++) {
+    if (i === length - 1) {
+      const dobbleChar = result.charAt(positionChar(i - 1));
+      const resultSplit = result.split("");
+      resultSplit.splice(positionChar(i), 0, dobbleChar);
+      result = resultSplit.join("");
+    } else result += characters.charAt(positionChar(charactersLength));
+  }
+  return result;
+}
+
+for (let i = 0; i < countPassword; i++) {
+    const root = window.document.getElementById("root");
+    const child = window.document.createElement("p");
+    child.innerText = Makeid(lengthPassword);
+    root.appendChild(child);
+  }
